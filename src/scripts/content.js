6ed9ffdbafd53f291d939d1f7e30e07487b64fd3
0kira <okira@hotmail.be>
2021-05-22 17:37:27 +0200

    dynamic good...

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -11,14 +11,38 @@ function contentGen() {
         return response.json()
     })
     .then((data) => {
+        var name = Object.keys(data)
         var count = Object.keys(data).length;
         for (let i = 0; i < count; i++) {
+
             let element = Object.keys(data)[i]
             let elementDOM = generativeElement[i]
             let eData = data[element];
-            //console.log(eData);
-            let rContent = eData[rN(0, eData.length-1)]
+
+
+            // Good morning if it's in the morning
+            if(name[i] == 'hello') {
+                
+                var currentTime = new Date();
+                var hours = currentTime.getHours();
+
+                if(hours >= 5 && hours <= 12) {
+                    var rContent = eData[0]
+                } else if(hours >= 12 && hours <= 17 ){
+                    var rContent = eData[1]
+                } else if(hours > 17){
+                    var rContent = eData[2]
+                }
+
+            } 
+            else {
+                var rContent = eData[rN(0, eData.length-1)]
+            }
             elementDOM.innerHTML = rContent
+            
+            //Debug
+            //console.log(eData);
+
         }
     })
     .catch((err) => {
