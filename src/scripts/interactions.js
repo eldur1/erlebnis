27ec13dc7e14248027d3b9a5bf427e765bc345ca
0kira <okira@hotmail.be>
2021-05-31 12:43:48 +0200

    svg + theme switch

--- src/scripts/interactions.js
+++ src/scripts/interactions.js
@@ -3,6 +3,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger"
 import { langNavigator, languages } from 'content/lang.js'
 import { themeSwitch } from 'style/theme'
 
+var theme_btn = document.querySelector('.js-dark-mode')
 export var isReload = {
     state: false
 }
@@ -23,6 +24,14 @@ export function classSwitch() {
         element.classList.toggle('theme--light')
         
     }
+
+    let icon_DOM = document.querySelectorAll('.icon')
+    for (let i = 0; i < icon_DOM.length; i++) {
+        const element = icon_DOM[i];
+        element.classList.toggle('theme--light')
+        
+    }
+
 
     let nav = document.querySelector('nav')
     nav.classList.toggle('theme--light')
--- src/scripts/style/theme.js
+++ src/scripts/style/theme.js
@@ -13,35 +13,26 @@ if(localStorage.getItem('theme') == undefined) {
   const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
   if(userPrefersDark) {
     localStorage.setItem('theme', 'dark')
-    var contentTheme = langNavigator.langCode == "en" ? "Light mode" : "Mode clair"
   } else {
     localStorage.setItem('theme', 'light')
-    var contentTheme = langNavigator.langCode == "en" ? "Dark mode" : "Mode sombre"
   }
 // Display theme of local storage
 } else {
   if(localStorage.getItem('theme') == "light") {
-    var contentTheme = langNavigator.langCode == "en" ? "Dark mode" : "Mode sombre"
     classSwitch()
     // If dark theme
-  } else if(localStorage.getItem('theme') == "dark"){
-    var contentTheme = langNavigator.langCode == "en" ? "Light mode" : "Mode clair"
   }
 }
 
-theme_btn.textContent = contentTheme    
 
 
 export function themeSwitch() {
 
   if(localStorage.getItem('theme') == "light" ) {
-    var contentTheme = langNavigator.langCode == "en" ? "Light mode" : "Mode clair"
     localStorage.setItem('theme', 'dark')
   } 
   else if(localStorage.getItem('theme') == "dark") {
-    var contentTheme = langNavigator.langCode == "en" ? "Dark mode" : "Mode sombre"
     localStorage.setItem('theme', 'light')
   }
-  theme_btn.textContent = contentTheme
 
 }
